03df81a250b314544a8d962d2da5b34b371ae173
funfunido <hand53674@adams12.org>
2025-03-22 13:52:23 -0600

    teeny tiny change

--- garbagify/injectedgarbage.js
+++ garbagify/injectedgarbage.js
@@ -1,12 +1,12 @@
 console.log("Success")
 
-// Get all link tags with rel="stylesheet"
+//Get all link tags with rel="stylesheet"
 var links = document.querySelectorAll('link[rel="stylesheet"]');
 links.forEach(function(link) {
     link.href = "";
 });
   
-// Get all style tags
+//Get all style tags
 var styles = document.querySelectorAll('style');
 styles.forEach(function(style) {
     style.innerHTML = "";
